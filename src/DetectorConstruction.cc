413afa429b62d263af8b61b1faf00b18d9ebe3ac
Zifeng Luo <87281041+luozf14@users.noreply.github.com>
2022-12-13 17:47:26 -0600

    construct geometry in scatttering chamber

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -25,12 +25,13 @@
 //
 //
 /// \file DetectorConstruction.cc
-/// \brief Implementation of the B1::DetectorConstruction class
+/// \brief Implementation of the TexPPACSim::DetectorConstruction class
 
 #include "DetectorConstruction.hh"
 
 #include "G4RunManager.hh"
 #include "G4NistManager.hh"
+#include "G4Tubs.hh"
 #include "G4Box.hh"
 #include "G4Cons.hh"
 #include "G4Orb.hh"
@@ -39,152 +40,215 @@
 #include "G4LogicalVolume.hh"
 #include "G4PVPlacement.hh"
 #include "G4SystemOfUnits.hh"
+#include "G4UnitsTable.hh"
+#include "G4UserLimits.hh"
 
 namespace TexPPACSim
 {
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+    //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-DetectorConstruction::DetectorConstruction()
-{}
+    DetectorConstruction::DetectorConstruction()
+    {
+    }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+    //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-DetectorConstruction::~DetectorConstruction()
-{}
+    DetectorConstruction::~DetectorConstruction()
+    {
+    }
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+    //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-G4VPhysicalVolume* DetectorConstruction::Construct()
-{
-  // Get nist material manager
-  G4NistManager* nist = G4NistManager::Instance();
+    G4VPhysicalVolume *DetectorConstruction::Construct()
+    {
+        printf("fTargetRotationAngle: %.1f\n", fTargetRotationAngle / M_PI * 180.);
+        printf("fTargetThickness: %.1f\n", fTargetThickness);
+        printf("fSiDetectorAngle: %.1f\n", fSiDetectorAngle / M_PI * 180.);
+        printf("fMdmAngle: %.1f\n", fMdmAngle / M_PI * 180.);
 
-  // Envelope parameters
-  //
-  G4double env_sizeXY = 20*cm, env_sizeZ = 30*cm;
-  G4Material* env_mat = nist->FindOrBuildMaterial("G4_WATER");
+        // Get nist material manager
+        G4NistManager *nist = G4NistManager::Instance();
 
-  // Option to switch on/off checking of volumes overlaps
-  //
-  G4bool checkOverlaps = true;
+        // Option to switch on/off checking of volumes overlaps
+        //
+        G4bool checkOverlaps = false;
 
-  //
-  // World
-  //
-  G4double world_sizeXY = 1.2*env_sizeXY;
-  G4double world_sizeZ  = 1.2*env_sizeZ;
-  G4Material* world_mat = nist->FindOrBuildMaterial("G4_AIR");
+        //
+        // World
+        //
+        G4double world_sizeXY = 2. * m;
+        G4double world_sizeZ = 2. * m;
+        G4Material *world_mat = nist->FindOrBuildMaterial("G4_Galactic");
 
-  G4Box* solidWorld =
-    new G4Box("World",                       //its name
-       0.5*world_sizeXY, 0.5*world_sizeXY, 0.5*world_sizeZ);     //its size
+        G4Box *solidWorld =
+            new G4Box("World",                                                    // its name
+                      0.5 * world_sizeXY, 0.5 * world_sizeXY, 0.5 * world_sizeZ); // its size
 
-  G4LogicalVolume* logicWorld =
-    new G4LogicalVolume(solidWorld,          //its solid
-                        world_mat,           //its material
-                        "World");            //its name
+        G4LogicalVolume *logicWorld =
+            new G4LogicalVolume(solidWorld, // its solid
+                                world_mat,  // its material
+                                "World");   // its name
 
-  G4VPhysicalVolume* physWorld =
-    new G4PVPlacement(0,                     //no rotation
-                      G4ThreeVector(),       //at (0,0,0)
-                      logicWorld,            //its logical volume
-                      "World",               //its name
-                      0,                     //its mother  volume
-                      false,                 //no boolean operation
-                      0,                     //copy number
-                      checkOverlaps);        //overlaps checking
+        G4VPhysicalVolume *physWorld =
+            new G4PVPlacement(0,               // no rotation
+                              G4ThreeVector(), // at (0,0,0)
+                              logicWorld,      // its logical volume
+                              "World",         // its name
+                              0,               // its mother  volume
+                              false,           // no boolean operation
+                              0,               // copy number
+                              checkOverlaps);  // overlaps checking
 
-  //
-  // Envelope
-  //
-  G4Box* solidEnv =
-    new G4Box("Envelope",                    //its name
-        0.5*env_sizeXY, 0.5*env_sizeXY, 0.5*env_sizeZ); //its size
+        //
+        // Target
+        //
+        G4Material *targetMaterial = new G4Material("Carbon", 2.253 * g / cm3, nist->FindOrBuildMaterial("G4_C"));
+        G4RotationMatrix *targetRot = new G4RotationMatrix;
+        targetRot->rotateY(fTargetRotationAngle);
+        G4double targetRMin = 0.;
+        G4double targetRMax = 3.5 * mm;
+        G4double targetDz = fTargetThickness / (2.253 * g / cm3);
+        G4cout << "targetDz: " << G4BestUnit(targetDz, "Length") << G4endl;
+        G4Tubs *solidTarget = new G4Tubs("Target",                                                      // its name
+                                         targetRMin, targetRMax, 0.5 * targetDz, 2. * M_PI, 2. * M_PI); // its size
 
-  G4LogicalVolume* logicEnv =
-    new G4LogicalVolume(solidEnv,            //its solid
-                        env_mat,             //its material
-                        "Envelope");         //its name
+        G4LogicalVolume *logicTarget =
+            new G4LogicalVolume(solidTarget,    // its solid
+                                targetMaterial, // its material
+                                "Target");      // its name
 
-  new G4PVPlacement(0,                       //no rotation
-                    G4ThreeVector(),         //at (0,0,0)
-                    logicEnv,                //its logical volume
-                    "Envelope",              //its name
-                    logicWorld,              //its mother  volume
-                    false,                   //no boolean operation
-                    0,                       //copy number
-                    checkOverlaps);          //overlaps checking
+        new G4PVPlacement(targetRot,       // rotation
+                          G4ThreeVector(), // at (0,0,0)
+                          logicTarget,     // its logical volume
+                          "Target",        // its name
+                          logicWorld,      // its mother  volume
+                          false,           // no boolean operation
+                          0,               // copy number
+                          checkOverlaps);  // overlaps checking
 
-  //
-  // Shape 1
-  //
-  G4Material* shape1_mat = nist->FindOrBuildMaterial("G4_A-150_TISSUE");
-  G4ThreeVector pos1 = G4ThreeVector(0, 2*cm, -7*cm);
+        G4double maxStep = 0.1 * targetDz;
+        logicTarget->SetUserLimits(new G4UserLimits(maxStep));
 
-  // Conical section shape
-  G4double shape1_rmina =  0.*cm, shape1_rmaxa = 2.*cm;
-  G4double shape1_rminb =  0.*cm, shape1_rmaxb = 4.*cm;
-  G4double shape1_hz = 3.*cm;
-  G4double shape1_phimin = 0.*deg, shape1_phimax = 360.*deg;
-  G4Cons* solidShape1 =
-    new G4Cons("Shape1",
-    shape1_rmina, shape1_rmaxa, shape1_rminb, shape1_rmaxb, shape1_hz,
-    shape1_phimin, shape1_phimax);
+        //
+        // Si detector - Delta E
+        //
+        G4ThreeVector DeltaEPos = G4ThreeVector(0., 0., 13.724 * cm);
+        DeltaEPos.setTheta(fSiDetectorAngle);
+        DeltaEPos.setPhi(180. * deg);
+        // Rotate the dE detector
+        G4RotationMatrix *DeltaERot = new G4RotationMatrix;
+        DeltaERot->rotateY(fSiDetectorAngle);
+        // Box shape
+        G4double DeltaEX = 5. * cm;
+        G4double DeltaEY = 5. * cm;
+        G4double DeltaEZ = 32. * um;
 
-  G4LogicalVolume* logicShape1 =
-    new G4LogicalVolume(solidShape1,         //its solid
-                        shape1_mat,          //its material
-                        "Shape1");           //its name
+        G4Box *solidDeltaE =
+            new G4Box("DetdE",
+                      0.5 * DeltaEX, 0.5 * DeltaEY, 0.5 * DeltaEZ);
 
-  new G4PVPlacement(0,                       //no rotation
-                    pos1,                    //at position
-                    logicShape1,             //its logical volume
-                    "Shape1",                //its name
-                    logicEnv,                //its mother  volume
-                    false,                   //no boolean operation
-                    0,                       //copy number
-                    checkOverlaps);          //overlaps checking
+        G4LogicalVolume *logicDeltaE =
+            new G4LogicalVolume(solidDeltaE,                        // its solid
+                                nist->FindOrBuildMaterial("G4_Si"), // its material
+                                "SiDetectorDeltaE");                // its name
 
-  //
-  // Shape 2
-  //
-  G4Material* shape2_mat = nist->FindOrBuildMaterial("G4_BONE_COMPACT_ICRU");
-  G4ThreeVector pos2 = G4ThreeVector(0, -1*cm, 7*cm);
+        new G4PVPlacement(DeltaERot,          // rotation
+                          DeltaEPos,          // at position
+                          logicDeltaE,        // its logical volume
+                          "SiDetectorDeltaE", // its name
+                          logicWorld,         // its mother  volume
+                          false,              // no boolean operation
+                          0,                  // copy number
+                          checkOverlaps);     // overlaps checking
 
-  // Trapezoid shape
-  G4double shape2_dxa = 12*cm, shape2_dxb = 12*cm;
-  G4double shape2_dya = 10*cm, shape2_dyb = 16*cm;
-  G4double shape2_dz  = 6*cm;
-  G4Trd* solidShape2 =
-    new G4Trd("Shape2",                      //its name
-              0.5*shape2_dxa, 0.5*shape2_dxb,
-              0.5*shape2_dya, 0.5*shape2_dyb, 0.5*shape2_dz); //its size
+        //
+        // Si detector - E
+        //
+        G4ThreeVector EPos = G4ThreeVector(0., 0., 14.224 * cm);
+        EPos.setTheta(fSiDetectorAngle);
+        EPos.setPhi(180. * deg);
+        // Rotate the dE detector
+        G4RotationMatrix *ERot = new G4RotationMatrix;
+        ERot->rotateY(fSiDetectorAngle);
+        // Box shape
+        G4double EX = 5. * cm;
+        G4double EY = 5. * cm;
+        G4double EZ = 500. * um;
 
-  G4LogicalVolume* logicShape2 =
-    new G4LogicalVolume(solidShape2,         //its solid
-                        shape2_mat,          //its material
-                        "Shape2");           //its name
+        G4Box *solidE =
+            new G4Box("DetdE",
+                      0.5 * EX, 0.5 * EY, 0.5 * EZ);
 
-  new G4PVPlacement(0,                       //no rotation
-                    pos2,                    //at position
-                    logicShape2,             //its logical volume
-                    "Shape2",                //its name
-                    logicEnv,                //its mother  volume
-                    false,                   //no boolean operation
-                    0,                       //copy number
-                    checkOverlaps);          //overlaps checking
+        G4LogicalVolume *logicE =
+            new G4LogicalVolume(solidE,                             // its solid
+                                nist->FindOrBuildMaterial("G4_Si"), // its material
+                                "SiDetectorE");                     // its name
 
-  // Set Shape2 as scoring volume
-  //
-  fScoringVolume = logicShape2;
+        new G4PVPlacement(ERot,           // rotation
+                          EPos,           // at position
+                          logicE,         // its logical volume
+                          "SiDetectorE",  // its name
+                          logicWorld,     // its mother  volume
+                          false,          // no boolean operation
+                          0,              // copy number
+                          checkOverlaps); // overlaps checking
 
-  //
-  //always return the physical World
-  //
-  return physWorld;
-}
+        //
+        // MDM slit box
+        //
+        G4double slitBoxDistance = 63.5 * cm;
+        G4ThreeVector slitBoxPos = G4ThreeVector(slitBoxDistance + 0.5 * mm, 0., 0.); // position in mother frame
+        slitBoxPos.setTheta(-1. * fMdmAngle);
+        slitBoxPos.setPhi(0. * deg);
+        G4RotationMatrix *slitBoxRot = new G4RotationMatrix;
+        slitBoxRot->rotateY(-1. * fMdmAngle);
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+        G4VSolid *solidSlitBox = new G4Box("SlitBox", 2.27965 * cm, 2.27965 * cm, 1. * mm); // measured on 1/26/2022
+        G4LogicalVolume *logicSlitBox = new G4LogicalVolume(solidSlitBox, nist->FindOrBuildMaterial("G4_Galactic"), "SlitBox");
+        new G4PVPlacement(slitBoxRot,     // rotation
+                          slitBoxPos,     // at position
+                          logicSlitBox,   // its logical volume
+                          "SlitBox",      // its name
+                          logicWorld,     // its mother  volume
+                          false,          // no boolean operation
+                          0,              // copy number
+                          checkOverlaps); // overlaps checking
+
+        //
+        // always return the physical World
+        //
+        return physWorld;
+    }
+
+    //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+    void DetectorConstruction::ParseParams(std::map<std::string, double> params)
+    {
+        G4cout << "-----In DetectorConstruction::ParseParams-----" << G4endl;
+        for (auto it : params)
+        {
+            if (it.first == "TargetRotationAngleInDeg")
+            {
+                fTargetRotationAngle = it.second * deg;
+                G4cout << "Set: Target rotation angle = " << G4BestUnit(fTargetRotationAngle, "Angle") << G4endl;
+            }
+            else if (it.first == "TargetThicknessInMgCm2")
+            {
+                fTargetThickness = it.second * mg / cm2;
+                G4cout << "Set: Target thickness = " << G4BestUnit(fTargetThickness, "Mass/Surface") << G4endl;
+            }
+            else if (it.first == "SiDetectorAngleInDeg")
+            {
+                fSiDetectorAngle = it.second * deg;
+                G4cout << "Set: Si detector angle = " << G4BestUnit(fSiDetectorAngle, "Angle") << G4endl;
+            }
+            else if (it.first == "MdmAngleInDeg")
+            {
+                fMdmAngle = it.second * deg;
+                G4cout << "Set: MDM angle = " << G4BestUnit(fMdmAngle, "Angle") << G4endl;
+            }
+        }
+    }
 
 }
