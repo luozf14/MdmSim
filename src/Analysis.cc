64005e8b4f3c40f3e2e65f11c58474301ddc2f4b
Zifeng Luo <87281041+luozf14@users.noreply.github.com>
2022-12-15 17:05:18 -0600

    correct .root name

--- src/Analysis.cc
+++ src/Analysis.cc
@@ -41,7 +41,7 @@ namespace TexPPACSim
         if (!fFactoryOn)
         {
             // Open a root file
-            std::string fileName = "Data~" + processNumber;
+            std::string fileName = "Data~" + processNumber + ".root";
             fFile = new TFile(fileName.c_str(), "RECREATE");
             if (!fFile)
             {
