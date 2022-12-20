07c1ba7230aa8054e2dd369e32752464039145ed
Zifeng Luo <87281041+luozf14@users.noreply.github.com>
2022-12-19 22:06:13 -0600

    add beam angular spreading

--- src/PrimaryGeneratorAction.cc
+++ src/PrimaryGeneratorAction.cc
@@ -13,50 +13,49 @@
 namespace TexPPACSim
 {
 
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+    //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-PrimaryGeneratorAction::PrimaryGeneratorAction()
-{
-  G4int n_particle = 1;
-  fParticleGun  = new G4ParticleGun(n_particle);
+    PrimaryGeneratorAction::PrimaryGeneratorAction()
+    {
+        G4int n_particle = 1;
+        fParticleGun = new G4ParticleGun(n_particle);
 
-  // default particle kinematic
-  G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
-  G4String particleName;
-  G4ParticleDefinition* particle
-    = particleTable->FindParticle(particleName="proton");
-  fParticleGun->SetParticleDefinition(particle);
-  fParticleGun->SetParticleMomentumDirection(G4ThreeVector(0.,0.,1.));
-  fParticleGun->SetParticleEnergy(40.*MeV);
+        // default particle kinematic
+        G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
+        G4String particleName;
+        G4ParticleDefinition *particle = particleTable->FindParticle(particleName = "proton");
+        fParticleGun->SetParticleDefinition(particle);
+        fParticleGun->SetParticleEnergy(40. * MeV);
+    }
+
+    //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+    PrimaryGeneratorAction::~PrimaryGeneratorAction()
+    {
+        delete fParticleGun;
+    }
+
+    //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+    void PrimaryGeneratorAction::GeneratePrimaries(G4Event *anEvent)
+    {
+        // this function is called at the begining of ecah event
+        //
+
+        // In order to avoid dependence of PrimaryGeneratorAction
+        // on DetectorConstruction class we get Envelope volume
+        // from G4LogicalVolumeStore.
+
+        G4ThreeVector direction(1.,0,0);
+        direction.setPhi(G4RandFlat::shoot(-180.*deg,180.*deg));
+        direction.setTheta(std::acos(G4RandFlat::shoot(std::cos(1.5*deg),1.)));
+        fParticleGun->SetParticleMomentumDirection(direction);
+
+        fParticleGun->SetParticlePosition(G4ThreeVector(0., 0., -1.));
+
+        fParticleGun->GeneratePrimaryVertex(anEvent);
+    }
+
+    //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
 }
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-PrimaryGeneratorAction::~PrimaryGeneratorAction()
-{
-  delete fParticleGun;
-}
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-void PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
-{
-  //this function is called at the begining of ecah event
-  //
-
-  // In order to avoid dependence of PrimaryGeneratorAction
-  // on DetectorConstruction class we get Envelope volume
-  // from G4LogicalVolumeStore.
-
-
-
-  fParticleGun->SetParticlePosition(G4ThreeVector(0.,0.,-1.));
-
-  fParticleGun->GeneratePrimaryVertex(anEvent);
-}
-
-//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
-}
-
-
