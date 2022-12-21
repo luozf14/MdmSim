0e0ffd220b139e009457b391586fdfccbd598b59
Zifeng Luo <87281041+luozf14@users.noreply.github.com>
2022-12-21 17:53:20 -0600

    GetFieldValue() now works even if the MDM is not at 0 deg

--- src/DipoleField.cc
+++ src/DipoleField.cc
@@ -10,48 +10,44 @@ namespace TexPPACSim
 
     //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-    DipoleField::DipoleField()
+    DipoleField::DipoleField(G4ThreeVector dipolePos) : fDipolePos(dipolePos)
     {
-        // define commands for this class
-        DefineCommands();
     }
 
     //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
     DipoleField::~DipoleField()
     {
-        delete fMessenger;
     }
 
     void DipoleField::GetFieldValue(const G4double PositionAndTime[4], double *bField) const
     {
         bField[0] = 0.;
-        G4ThreeVector dipolePos(-kDipoleFieldRadius, 0., kFirstArmLength);
-        G4ThreeVector particlePos(PositionAndTime[0],PositionAndTime[1],PositionAndTime[2]);
-        G4ThreeVector deltaR = particlePos-dipolePos;
+        G4ThreeVector particlePos(PositionAndTime[0], PositionAndTime[1], PositionAndTime[2]);
+        G4ThreeVector deltaR = particlePos - fDipolePos;
         G4double x = std::sqrt(deltaR.x() * deltaR.x() + deltaR.z() * deltaR.z()) - kDipoleFieldRadius;
-        G4double By = fBy0 * (1. - kDipoleAlpha * (x / kDipoleFieldRadius) - 0.04 * (x / kDipoleFieldRadius) * (x / kDipoleFieldRadius));
+        G4double By = fBy0 * (1. - kDipoleNDX * (x / kDipoleFieldRadius) + kDipoleBET1 * std::pow(x / kDipoleFieldRadius, 2.) + kDipoleGAMA * std::pow(x / kDipoleFieldRadius, 3.) + kDipoleDELT * std::pow(x / kDipoleFieldRadius, 4.));
         bField[1] = By;
         bField[2] = 0.;
     }
 
     //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+    /*
+        void DipoleField::DefineCommands()
+        {
+            // Define /TexPPACSim/field command directory using generic messenger class
+            fMessenger = new G4GenericMessenger(this,
+                                                "/TexPPACSim/field/",
+                                                "Field control");
 
-    void DipoleField::DefineCommands()
-    {
-        // Define /TexPPACSim/field command directory using generic messenger class
-        fMessenger = new G4GenericMessenger(this,
-                                            "/TexPPACSim/field/",
-                                            "Field control");
-
-        // fieldValue command
-        auto &valueCmd = fMessenger->DeclareMethodWithUnit("value", "tesla",
-                                                           &DipoleField::SetField,
-                                                           "Set field strength.");
-        valueCmd.SetParameterName("field", true);
-        valueCmd.SetDefaultValue(".375");
-    }
-
+            // fieldValue command
+            auto &valueCmd = fMessenger->DeclareMethodWithUnit("value", "tesla",
+                                                               &DipoleField::SetField,
+                                                               "Set field strength.");
+            valueCmd.SetParameterName("field", true);
+            valueCmd.SetDefaultValue(".375");
+        }
+    */
     //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 }
--- src/FirstMultipoleField.cc
+++ src/FirstMultipoleField.cc
@@ -3,6 +3,7 @@
 
 #include "G4GenericMessenger.hh"
 #include "G4SystemOfUnits.hh"
+#include "G4RotationMatrix.hh"
 #include "globals.hh"
 
 namespace TexPPACSim
@@ -10,7 +11,7 @@ namespace TexPPACSim
 
     //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-    FirstMultipoleField::FirstMultipoleField()
+    FirstMultipoleField::FirstMultipoleField(G4ThreeVector pos, G4RotationMatrix* rot) : fFirstMultipolePos(pos), fFirstMultipoleRot(rot)
     {
     }
 
@@ -20,23 +21,36 @@ namespace TexPPACSim
     {
     }
 
+    //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+    void FirstMultipoleField::SetField(G4double multipoleProbe)
+    {
+        fBQR = -1. * multipoleProbe * kJeffParameters[5];
+        fBHR = fBQR * kJeffParameters[1] / kJeffParameters[0];
+        fBOR = fBQR * kJeffParameters[2] / kJeffParameters[0];
+        fBDR = fBQR * kJeffParameters[3] / kJeffParameters[0];
+        fBDDR = fBQR * kJeffParameters[4] / kJeffParameters[0];
+    }
+
+    //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
     void FirstMultipoleField::GetFieldValue(const G4double PositionAndTime[4], G4double *bField) const
     {
-        G4double multipoleHallProbe = 2780.84;
-        G4double fBQR = -1. * multipoleHallProbe * 1e-4 * kJeffParameters[5];
-        G4double fBHR = fBQR * kJeffParameters[1] / kJeffParameters[0];
-        G4double fBOR = fBQR * kJeffParameters[2] / kJeffParameters[0];
-        G4double fBDR = fBQR * kJeffParameters[3] / kJeffParameters[0];
-        G4double fBDDR = fBQR * kJeffParameters[4] / kJeffParameters[0];
-
         G4double G1 = fBQR / std::pow(kFirstMultipoleAperture, 1.);
         G4double G2 = fBHR / std::pow(kFirstMultipoleAperture, 2.);
         G4double G3 = fBOR / std::pow(kFirstMultipoleAperture, 3.);
         G4double G4 = fBDR / std::pow(kFirstMultipoleAperture, 4.);
         G4double G5 = fBDDR / std::pow(kFirstMultipoleAperture, 5.);
+        // G4cout << "\n---> FirstMultipoleField::GetFieldValue: fFirstMultipolePos= " << G4BestUnit(fFirstMultipolePos, "Length") << G4endl;
 
-        G4double x = -PositionAndTime[0];
-        G4double y = PositionAndTime[1];
+        G4ThreeVector particlePos(PositionAndTime[0], PositionAndTime[1], PositionAndTime[2]);
+        // G4cout << "\n---> FirstMultipoleField::GetFieldValue: particlePos= " << G4BestUnit(particlePos, "Length") << G4endl;
+        G4ThreeVector translatedR = particlePos - fFirstMultipolePos;
+        G4ThreeVector rotatedR = fFirstMultipoleRot->inverse().operator()(translatedR);
+        G4double x = rotatedR.x();
+        G4double y = rotatedR.y();
+        // G4cout << "\n---> FirstMultipoleField::GetFieldValue: rotatedR= " << G4BestUnit(rotatedR, "Length") << G4endl;
+
 
         bField[0] = G1 * y + G2 * (2. * x * y) + G3 * (3. * std::pow(x, 2.) * y - std::pow(y, 3.)) + G4 * 4. * (std::pow(x, 3.) * y - x * std::pow(y, 3.)) + G5 * (5. * std::pow(x, 4.) * y - 10. * std::pow(x, 2.) * std::pow(y, 3.) + std::pow(y, 5.));
         bField[1] = G1 * x + G2 * (std::pow(x, 2.) - std::pow(y, 2.)) + G3 * (std::pow(x, 3.) - 3. * x * std::pow(y, 2.)) + G4 * (std::pow(x, 4.) - 6. * std::pow(x * y, 2.) + std::pow(y, 4.)) + G5 * (std::pow(x, 5.) - 10. * std::pow(x, 3.) * std::pow(y, 2.) + 5. * x * std::pow(y, 4.));
