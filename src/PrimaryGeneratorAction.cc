d6559ad46ad01371f99b34f62454dd782554f83e
Zifeng Luo <87281041+luozf14@users.noreply.github.com>
2023-01-03 16:56:06 -0600

    energy spread; angular distribution

--- src/PrimaryGeneratorAction.cc
+++ src/PrimaryGeneratorAction.cc
@@ -25,7 +25,6 @@ namespace TexPPACSim
         G4String particleName;
         G4ParticleDefinition *particle = particleTable->FindParticle(particleName = "proton");
         fParticleGun->SetParticleDefinition(particle);
-        fParticleGun->SetParticleEnergy(20. * MeV);
     }
 
     //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
@@ -46,12 +45,13 @@ namespace TexPPACSim
         // on DetectorConstruction class we get Envelope volume
         // from G4LogicalVolumeStore.
 
-        G4ThreeVector direction(1.,0,0);
-        direction.setPhi(G4RandFlat::shoot(-180.*deg,180.*deg));
-        direction.setTheta(std::acos(G4RandFlat::shoot(std::cos(2.*deg),1.)));
+        G4ThreeVector direction(1., 0, 0);
+        direction.setPhi(G4RandFlat::shoot(-180 * deg, 180 * deg));
+        direction.setTheta(std::acos(G4RandFlat::shoot(std::cos(1.5 * deg), std::cos(0. * deg))));
         fParticleGun->SetParticleMomentumDirection(direction);
 
         fParticleGun->SetParticlePosition(G4ThreeVector(0., 0., -1.));
+        fParticleGun->SetParticleEnergy(G4RandGauss::shoot(20. * MeV, 0.005 * 20. * MeV / 2.355));
 
         fParticleGun->GeneratePrimaryVertex(anEvent);
     }
