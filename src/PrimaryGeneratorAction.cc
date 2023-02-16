e9f0571623b70d7882a4586a98970d9ab3539c63
Zifeng Luo <87281041+luozf14@users.noreply.github.com>
2023-02-16 13:39:45 -0600

    test

--- src/PrimaryGeneratorAction.cc
+++ src/PrimaryGeneratorAction.cc
@@ -6,9 +6,11 @@
 #include "G4RunManager.hh"
 #include "G4ParticleGun.hh"
 #include "G4ParticleTable.hh"
+#include "G4IonTable.hh"
 #include "G4ParticleDefinition.hh"
 #include "G4SystemOfUnits.hh"
 #include "Randomize.hh"
+#include "G4ChargedGeantino.hh"
 
 namespace MdmSim
 {
@@ -22,8 +24,7 @@ namespace MdmSim
 
         // default particle kinematic
         G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
-        G4String particleName;
-        G4ParticleDefinition *particle = particleTable->FindParticle(particleName = "proton");
+        G4ParticleDefinition *particle = particleTable->FindParticle("chargedgeantino");
         fParticleGun->SetParticleDefinition(particle);
     }
 
@@ -44,14 +45,34 @@ namespace MdmSim
         // In order to avoid dependence of PrimaryGeneratorAction
         // on DetectorConstruction class we get Envelope volume
         // from G4LogicalVolumeStore.
+        G4ParticleDefinition *particle = fParticleGun->GetParticleDefinition();
+        if (particle == G4ChargedGeantino::ChargedGeantino())
+        {
+            G4ParticleDefinition *ion;
+            G4ParticleTable *particleTable = G4ParticleTable::GetParticleTable();
+            G4int Z = 1, A = 1;
+            if (particleTable->GetIonTable()->FindIon(Z, A, 0.0))
+                ion = particleTable->GetIonTable()->FindIon(Z, A, 0.0);
+            else
+                ion = particleTable->GetIonTable()->GetIon(Z, A, 0.0);
+            // ion = particleTable->FindParticle("alpha");
+            fParticleGun->SetParticleDefinition(ion);
+        }
 
-        G4ThreeVector direction(1., 0, 0);
-        direction.setPhi(G4RandFlat::shoot(-180 * deg, 180 * deg));
-        direction.setTheta(std::acos(G4RandFlat::shoot(std::cos(1.5 * deg), std::cos(0. * deg))));
+        G4double xAngle = G4RandFlat::shoot(-8. * deg, 8. * deg);
+        G4double yAngle = G4RandFlat::shoot(-4. * deg, 4. * deg);
+        // G4double yAngle = 0.;
+        G4ThreeVector direction(std::tan(xAngle), std::tan(yAngle) * std::sqrt(std::tan(xAngle) * std::tan(xAngle) + 1.), 1.);
+        // G4ThreeVector direction(0., 0., 1.);
+        // direction.setTheta(std::acos(G4RandFlat::shoot(std::cos(8 * deg), std::cos(0. * deg))));
+        // G4double phi = G4RandFlat::shoot(-180. * deg, 180. * deg);
+        // G4double phi = G4RandFlat::shoot() > 0.5 ? 90. * deg : -90. * deg;
+        // direction.setPhi(phi);
         fParticleGun->SetParticleMomentumDirection(direction);
 
         fParticleGun->SetParticlePosition(G4ThreeVector(0., 0., -1.));
-        fParticleGun->SetParticleEnergy(G4RandGauss::shoot(20. * MeV, 0.005 * 20. * MeV / 2.355));
+        // fParticleGun->SetParticleEnergy(G4RandGauss::shoot(20. * MeV, 0.005 * 20. * MeV / 2.355));
+        fParticleGun->SetParticleEnergy(20. * MeV);
 
         fParticleGun->GeneratePrimaryVertex(anEvent);
     }
